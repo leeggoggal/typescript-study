13b310442d5e207e6f70de164187fa4c227fcc68
LSJ <tltlgns9@naver.com>
2025-04-21 10:19:02 +0900

    TODO 주석 라인 추가

--- example.js
+++ example.js
@@ -1,3 +1,8 @@
+// TODO: divide 라고 하는 함수를 만들고
+// TODO: Item 이라고 하는 커스텀 타입을 만들어서
+// TODO: 이 item의 타입에 따라, 그리고 어떤 값인지에 따라
+// TODO: 각각의 값을 구분하여 console.log()로 출력하는 함수를 만들어보세요
+// ? enum 타입 활용, switch문 활용
 var cart = [
   { name: 'egg', category: 'food', price: 1000, count: 5 },
   { name: 'zero_coke', category: 'drink', price: 3000, count: 2 },
