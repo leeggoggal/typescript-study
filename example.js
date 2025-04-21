bd13c102bd9164f77977825ad568f6e6accc8d3b
LSJ <tltlgns9@naver.com>
2025-04-21 10:18:09 +0900

    TODO 주석 라인 삭제

---
+++ example.js
@@ -0,0 +1,11 @@
+var cart = [
+  { name: 'egg', category: 'food', price: 1000, count: 5 },
+  { name: 'zero_coke', category: 'drink', price: 3000, count: 2 },
+  { name: 'tobaco', category: 'smoking', price: 4500, count: 1 },
+  { name: 'cup', category: 'etc', price: 7000, count: 8 },
+];
+console.log(cart[0]);
+/* const calcTotal = () => {};
+
+const total = calcTotal(cart);
+console.log('total:', total); */
